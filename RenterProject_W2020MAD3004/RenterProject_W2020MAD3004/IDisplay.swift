09828d00c19321e5e0b0854b65eba9b24072775e
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-10 20:22:39 -0500

    Added protocol

---
+++ RenterProject_W2020MAD3004/RenterProject_W2020MAD3004/IDisplay.swift
@@ -0,0 +1,15 @@
+//
+//  IDisplay.swift
+//  RenterProject_W2020MAD3004
+//
+//  Created by MacStudent on 2020-02-10.
+//  Copyright © 2020 Group1. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplayDelegate
+{
+    func display()
+    
+}
---
+++ RenterProject_W2020MAD3004/RenterProject_W2020MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RenterProject_W2020MAD3004
+//
+//  Created by MacStudent on 2020-02-10.
+//  Copyright © 2020 Group1. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
